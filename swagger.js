36ee90f4ee8264a62c095c6ccf21044ccfb8e863
matthewfelton <77081600+matthewfelton@users.noreply.github.com>
2024-02-06 15:20:11 -0600

    added more file structure and swagger basics

---
+++ swagger.js
@@ -0,0 +1,21 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'moviebuffhub API',
+        description: 'moviebuffhub API',
+    },
+    host: 'https://moviebuffhub.onrender.com',
+    schemes: ['https'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//   await import('./index.js');
+// });
