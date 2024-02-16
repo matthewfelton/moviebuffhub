b3c09318877c4f027efcd9a3eba4cb9443366246
matthewfelton <77081600+matthewfelton@users.noreply.github.com>
2024-02-15 19:04:10 -0600

    changed notempy to required :D

--- middleware/validate.js
+++ middleware/validate.js
@@ -2,12 +2,12 @@ const validator = require('../helpers/validate');
 
 const savemovie = (req, res, next) => {
     const validationRule = {
-        title: 'notEmpty|string|withMessage:Title is required.',
-        genre: 'notEmpty|string|withMessage:Genre is required.',
-        rating: 'notEmpty|string|withMessage:Rating is required.',
-        runTime: 'notEmpty|string|withMessage:Run time is required.',
+        title: 'required|string|withMessage:Title is required.',
+        genre: 'required|string|withMessage:Genre is required.',
+        rating: 'required|string|withMessage:Rating is required.',
+        runTime: 'required|string|withMessage:Run time is required.',
         releaseYear: 'isNumeric|isLength:{min:4,max:4}|withMessage:Invalid release year.',
-        director: 'notEmpty|string|withMessage:Director is required.',
+        director: 'required|string|withMessage:Director is required.',
         metascore: 'isFloat:{min:0,max:100}|withMessage:metascore is not an acceptable value.'
     };
     validator(req.body, validationRule, {}, (err, status) => {
@@ -25,11 +25,11 @@ const savemovie = (req, res, next) => {
 
 const savetv = (req, res, next) => {
     const validationRule = {
-        title: 'notEmpty|string|withMessage:Title is required.',
-        genre: 'notEmpty|string|withMessage:Genre is required.',
-        rating: 'notEmpty|string|withMessage:Rating is required.',
-        runTime: 'notEmpty|string|withMessage:Run time is required.',
-        airTime: 'notEmpty|string|withMessage:Air time is required.',
+        title: 'required|string|withMessage:Title is required.',
+        genre: 'required|string|withMessage:Genre is required.',
+        rating: 'required|string|withMessage:Rating is required.',
+        runTime: 'required|string|withMessage:Run time is required.',
+        airTime: 'required|string|withMessage:Air time is required.',
     };
     validator(req.body, validationRule, {}, (err, status) => {
         if (!status) {
