82ae30b193e133d8afc6446b5953e9fa9e620211
matthewfelton <77081600+matthewfelton@users.noreply.github.com>
2024-02-18 21:37:32 -0600

    added basic 0auth items for testing

--- server.js
+++ server.js
@@ -11,6 +11,27 @@ const mongodb = require('./db/connection');
 // attempts to pull port or default to 8080
 const port = process.env.port || 8080;
 
+const { auth } = require('express-openid-connect');
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+
 app
     .use(cors({
         origin: '*', // Replace with your frontend domain or '*' for any origin
